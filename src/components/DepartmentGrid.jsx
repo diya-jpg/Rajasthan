fc0466c6b3f69919dc4bd0b7b02c3f616190efa7
diya-jpg <diyabari2004@gmail.com>
2026-06-19 12:14:15 +0530

    eight commit

--- src/components/DepartmentGrid.jsx
+++ src/components/DepartmentGrid.jsx
@@ -18,42 +18,50 @@ function DepartmentGrid({
 
   const departments = [
     {
+      name: "Revenue",
+      description: "Land records & certificates",
+      icon: <FiHome />,
+      iconClass: "revenue-icon"
+    },
+    {
       name: "Education",
+      description: "Academic records & certificates",
       icon: <FiBook />,
       iconClass: "education-icon"
     },
     {
       name: "Transport",
+      description: "Driving license & RC",
       icon: <FiTruck />,
       iconClass: "transport-icon"
     },
     {
       name: "Jan Aadhaar",
+      description: "Identity documents",
       icon: <FiUsers />,
       iconClass: "jan-icon"
     },
     {
       name: "Employment",
+      description: "Employment records",
       icon: <FiBriefcase />,
       iconClass: "employment-icon"
     },
     {
       name: "Health",
+      description: "Health records",
       icon: <FiHeart />,
       iconClass: "health-icon"
     },
     {
-      name: "Revenue",
-      icon: <FiHome />,
-      iconClass: "revenue-icon"
-    },
-    {
       name: "SSO",
+      description: "SSO services",
       icon: <FiGrid />,
       iconClass: "sso-icon"
     },
     {
       name: "Other",
+      description: "Other documents",
       icon: <FiLayers />,
       iconClass: "other-icon"
     }
@@ -82,14 +90,26 @@ function DepartmentGrid({
             }
           >
 
-            <div
-              className={`department-icon ${item.iconClass}`}
-            >
-              {item.icon}
+            <div className="department-left">
+
+              <div
+                className={`department-icon ${item.iconClass}`}
+              >
+                {item.icon}
+              </div>
+
+              <div className="department-info">
+
+                <h4>{item.name}</h4>
+
+                <p>{item.description}</p>
+
+              </div>
+
             </div>
 
-            <div className="department-name">
-              {item.name}
+            <div className="department-arrow">
+              →
             </div>
 
           </div>
