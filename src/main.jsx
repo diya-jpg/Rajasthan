e906da1309dfc267dd129808b535dd1a92cea7c0
diya-jpg <diyabari2004@gmail.com>
2026-06-23 12:02:14 +0530

    Tenth commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/new_citizen">
     <App />
   </BrowserRouter>
 );
